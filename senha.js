087fec43aebd1290718e52aceb8739c4d006f948
Jayneanderson Santos <aprenderprogramacao001@gmail.com>
2021-06-15 23:39:48 -0300

    Iniciado o desenvolvimento do footer

--- senha.js
+++ senha.js
@@ -1,33 +1,4 @@
-/* function caractere() {
 
-    //Se o meu input estiver vazio, eu quero que o typing seja removido
-    //mas se eu digitar e tiver um valor, eu quero que meu typing seja colocado
-    var getInput = document.getElementById('email')
-    var getInputValue = document.getElementById('email').value
-    document.getElementById('text').classList.add('typing-span')
-    getInput.classList.add('typing-input')
-
-    if (getInputValue == "" || getInputValue.length == 0) {
-        getInput.classList.remove('typing-input')
-        document.getElementById('text').classList.remove('typing-span')
-    }
-}
-
-function caracterePassword() {
-
-    //Se o meu input estiver vazio, eu quero que o typing seja removido
-    //mas se eu digitar e tiver um valor, eu quero que meu typing seja colocado
-    var getInput = document.getElementById('senha')
-    var getInputValue = document.getElementById('senha').value
-    document.getElementById('textSenha').classList.add('typing-span')
-    getInput.classList.add('typing-input')
-
-    if (getInputValue == "" || getInputValue.length == 0) {
-        getInput.classList.remove('typing-input')
-        document.getElementById('textSenha').classList.remove('typing-span')
-    }
-}
- */
 const getElement = document.querySelectorAll('.data')
 
 getElement.forEach(item => item.addEventListener('keyup', function () {
@@ -45,4 +16,10 @@ getElement.forEach(item => item.addEventListener('keyup', function () {
         item.previousElementSibling.classList.remove('typing-span')
     }
 }))
+
+
+/* 
+    O input do e-mail só será desbloqueado se o input de senha
+    tiver no mínimo 8 caracteres
+*/
 
