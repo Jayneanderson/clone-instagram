8b34329d253ce4fc2807db19b3d51db4007ce21d
Jayneanderson Santos <aprenderprogramacao001@gmail.com>
2021-06-18 23:46:37 -0300

    Desenvolvida a lógica da regra de negócio de permitir login se houver, no mínimo  8 caracteres na senha e, no mínimo, 1 caractere no e-mail.

--- senha.js
+++ senha.js
@@ -23,3 +23,26 @@ getElement.forEach(item => item.addEventListener('keyup', function () {
     tiver no mínimo 8 caracteres
 */
 
+const input = document.querySelector('#email')
+const inputSenha = document.querySelector('#senha')
+
+function validarCampo() { 
+    const inputValue = input.value
+    const senhaValue = document.querySelector('#senha')
+
+    console.log(senhaValue)
+    if(inputValue.length > 0 && senhaValue.value.length > 7) {
+
+        const button = document.querySelector('.login-button')
+        button.removeAttribute('disabled')
+    }
+    
+    else {
+        const button = document.querySelector('.login-button')
+        button.setAttribute('disabled', 'disabled')
+    }
+}
+
+input.addEventListener('keyup', validarCampo)
+
+inputSenha.addEventListener('keyup', validarCampo);
